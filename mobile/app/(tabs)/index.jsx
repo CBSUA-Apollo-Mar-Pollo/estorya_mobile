fbe4ff4224461378c22e2a824947e316e4ef5f3e
CBSUA-Apollo-Mar-Pollo <103560480+CBSUA-Apollo-Mar-Pollo@users.noreply.github.com>
2025-03-23 20:17:23 +0800

    update
    - added tabs and main screens.

---
+++ mobile/app/(tabs)/index.jsx
@@ -0,0 +1,20 @@
+import { Link, useRouter } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
+import { Search, User } from "lucide-react-native";
+
+export default function Index() {
+  const router = useRouter();
+  return (
+    <View className="bg-white h-full">
+      <View className="flex-row items-center justify-between pt-2 pb-3 px-4 bg-white  border-b border-neutral-100">
+        <Text className="text-4xl font-extrabold">Estorya</Text>
+        <View className="flex-row gap-x-4 items-center">
+          <Search color="black" size={20} />
+          <TouchableOpacity onPress={() => router.push("/signin")}>
+            <Text className="text-xl">Sign in</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}

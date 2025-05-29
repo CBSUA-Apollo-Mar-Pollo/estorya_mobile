34ddf61131e5a4bdbb4dc38dc7a2f34841fe4a28
CBSUA-Apollo-Mar-Pollo <103560480+CBSUA-Apollo-Mar-Pollo@users.noreply.github.com>
2025-05-29 18:35:31 +0800

    update
    - added horizontal background color picker in bottom sheet add post.

--- mobile/components/post/bottom-sheet-add-post.jsx
+++ mobile/components/post/bottom-sheet-add-post.jsx
@@ -9,6 +9,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { ChevronLeft, Plus, Triangle } from "lucide-react-native";
 import { Icons } from "../utils/Icons";
 import { images } from "../../constants/images";
+import { FlatList } from "react-native-gesture-handler";
 
 const BottomSheetAddPost = ({ session, bottomSheetAddPostRef }) => {
   const snapPoints = useMemo(() => ["100%"], []);
@@ -24,6 +25,29 @@ const BottomSheetAddPost = ({ session, bottomSheetAddPostRef }) => {
     ),
     []
   );
+
+  const backgroundColors = [
+    { name: "white", hex: "white" },
+    { name: "Pale Lemon", hex: "#F7FFD1" },
+    { name: "Neon Lime", hex: "#B7FF33" },
+    { name: "Olive Green", hex: "#5A7F19" },
+    { name: "Mint Cream", hex: "#D6FFCC" },
+    { name: "Bright Spring Green", hex: "#44CB46" },
+    { name: "Forest Green", hex: "#237B2F" },
+    { name: "Pale Aqua", hex: "#DFF9F0" },
+    { name: "Aquamarine", hex: "#32CBB0" },
+    { name: "Deep Teal", hex: "#008970" },
+    { name: "Sky Blue", hex: "#8BE0FF" },
+    { name: "Vivid Cyan", hex: "#00AAFF" },
+    { name: "Deep Ocean Blue", hex: "#014C75" },
+    { name: "Lavender", hex: "#DCCBFF" },
+    { name: "Medium Violet", hex: "#7B69FF" },
+    { name: "Deep Indigo", hex: "#3F327B" },
+    { name: "Pastel Pink", hex: "#FFCCFF" },
+    { name: "Hot Pink", hex: "#FF5CD6" },
+    { name: "Magenta", hex: "#8C268B" },
+    { name: "Deep Magenta", hex: "#821B5F" },
+  ];
 
   return (
     <BottomSheetModal
@@ -107,9 +131,47 @@ const BottomSheetAddPost = ({ session, bottomSheetAddPostRef }) => {
             />
           </View>
 
+          {/* background color choices */}
+          <View className="flex-2 flex-row items-center gap-x-2 mx-2">
+            <TouchableOpacity
+              style={{ borderWidth: 0.2 }}
+              className="mb-2 px-1 py-1.5 rounded-lg "
+            >
+              <ChevronLeft size={30} color="black" />
+            </TouchableOpacity>
+            <FlatList
+              data={backgroundColors}
+              keyExtractor={(item) => item.hex}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              renderItem={({ item }) => (
+                <View
+                  className="w-12 mr-4 rounded-md justify-center items-center  "
+                  style={{
+                    borderWidth: 0.2,
+                    backgroundColor: item.hex,
+                    height: 38,
+                    marginBottom: 10,
+                    shadowColor: "rgba(0, 0, 0, 1)",
+                    shadowOffset: { width: 1, height: 1 },
+                    shadowOpacity: 0.5,
+                    shadowRadius: 6,
+                    elevation: 2,
+                  }}
+                >
+                  <Text></Text>
+                </View>
+              )}
+            />
+          </View>
+
           <View
-            style={{ paddingHorizontal: 40, paddingVertical: 7 }}
-            className="flex-2 flex-row justify-between  items-center px-10"
+            style={{
+              paddingHorizontal: 40,
+              paddingVertical: 10,
+              borderTopWidth: 0.4,
+            }}
+            className="flex-2 flex-row justify-between  items-center px-10 border-t"
           >
             <Image
               source={images.addPhotoToPost}
